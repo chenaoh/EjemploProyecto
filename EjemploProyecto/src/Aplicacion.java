442659fc5f6fb61102bb532a7e64f6136885e4ef
chenaoh <cdhenao@gmail.com>
2018-02-27 17:12:37 -0500

    Se Modifica la clase Aplicación, se agrega comentario de inicio y de fin...

--- EjemploProyecto/src/Aplicacion.java
+++ EjemploProyecto/src/Aplicacion.java
@@ -8,9 +8,16 @@ public class Aplicacion {
 		
 		Persona miPersona=new Persona("Pepito Perez",23,"Estudiante","7584632");
 		
+		System.out.println("DATOS DEL USUARIO...");
+		System.out.println("**********************************************");
+		
 		System.out.println("Nombre: "+miPersona.getNombre());
 		System.out.println("Edad: "+miPersona.getEdad());
 		System.out.println("Profesion: "+miPersona.getProfesion());
 		System.out.println("Telefono: "+miPersona.getTelefono());
+		
+		System.out.println("Se termina el sistema!!!");
+		
+		System.out.println("**********************************************");
 	}
 }
