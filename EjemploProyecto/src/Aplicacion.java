6c4e8f400c6bfcca136bfd7d8cf3a89b78158544
Cristin D Henao <36899241+cdhenao2@users.noreply.github.com>
2018-02-27 16:44:57 -0500

    Se agregan los asteriscos desde el otro usuario - cdhenao2

--- EjemploProyecto/src/Aplicacion.java
+++ EjemploProyecto/src/Aplicacion.java
@@ -8,9 +8,13 @@ public class Aplicacion {
 		
 		Persona miPersona=new Persona("Pepito Perez",23,"Estudiante","7584632");
 		
+		System.out.println("**********************************************");
+		
 		System.out.println("Nombre: "+miPersona.getNombre());
 		System.out.println("Edad: "+miPersona.getEdad());
 		System.out.println("Profesion: "+miPersona.getProfesion());
 		System.out.println("Telefono: "+miPersona.getTelefono());
+		
+		System.out.println("**********************************************");
 	}
 }
