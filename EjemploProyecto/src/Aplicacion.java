25cefac39f64f5cfd29f048806581446223cc01e
Cristin D Henao <36899241+cdhenao2@users.noreply.github.com>
2018-02-27 18:11:32 -0500

    Se agregan cambios

--- EjemploProyecto/src/Aplicacion.java
+++ EjemploProyecto/src/Aplicacion.java
@@ -7,14 +7,21 @@ public class Aplicacion {
 		JOptionPane.showMessageDialog(null, "Se presentará la info de la persona!");
 		
 		Persona miPersona=new Persona("Pepito Perez",23,"Estudiante","7584632");
-		
+
 		System.out.println("**********************************************");
-		
+		System.out.println("-----------------");
 		System.out.println("Nombre: "+miPersona.getNombre());
 		System.out.println("Edad: "+miPersona.getEdad());
 		System.out.println("Profesion: "+miPersona.getProfesion());
 		System.out.println("Telefono: "+miPersona.getTelefono());
 		
 		System.out.println("**********************************************");
+		
+		
+		System.out.println("++++++++++++++++++++++++");
+		System.out.println("++++++++++++++++++++++++");
+		System.out.println("++++++++++++++++++++++++");
+		System.out.println("++++++++++++++++++++++++");
+		System.out.println("++++++++++++++++++++++++");
 	}
 }
